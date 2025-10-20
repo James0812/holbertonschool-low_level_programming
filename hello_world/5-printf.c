e0d51205b324ed699638581caf198edd7cd602ad
james.roussel008@gmail.com <james.roussel008@gmail.com>
2025-10-20 11:38:48 +0200

    reponse

---
+++ hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints a piece of art using printf
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("  *     *  \n");
+	printf(" ***   *** \n");
+	printf("***********\n");
+	printf("   ***   \n");
+	printf("    *\n");
+	return (0);
+}
+
