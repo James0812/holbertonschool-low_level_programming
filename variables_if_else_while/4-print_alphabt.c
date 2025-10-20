5de8be15ad1ce28d942171234e1d625841bdc9d1
james.roussel008@gmail.com <james.roussel008@gmail.com>
2025-10-20 14:56:20 +0200

    reponse

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main prints the alphabet in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{ 
+
+     char c;
+
+
+     /* print alphabet in lowercase */
+     for (c = 'a'; c <='z'; c++)
+	 
+     putchar(c)
+
+
+     /* Print newline */
+
+     putchar('\n');
+
+     Return (0)
+}
