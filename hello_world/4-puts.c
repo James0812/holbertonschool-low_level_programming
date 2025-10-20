41e0705ba4703a9c5ff8f8bcca04dff72b725e83
james.roussel008@gmail.com <james.roussel008@gmail.com>
2025-10-20 10:38:52 +0200

    reponse

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,14 @@
-#!/bin/bash
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Affiche un message avec puts
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    puts("Programmer, c'est comme construire un puzzle multilingue");
-    return 0;
+	puts("Programmer, c'est comme construire un puzzle multilingue");
+	return (0);
 }
