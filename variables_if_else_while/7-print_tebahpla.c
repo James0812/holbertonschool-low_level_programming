6eed4be00cc3ddb7f1d12d1ba1668f1607ec82cd
james.roussel008@gmail.com <james.roussel008@gmail.com>
2025-10-20 15:51:55 +0200

    reponse

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -2,16 +2,19 @@
 
 /**
  * main - prints the lowercase alphabet in reverse
+ *
  * Return: 0
  */
 int main(void)
 {
-    char c;
+	char c;
 
-    for (c = 'z'; c >= 'a'; c--)
-        putchar(c);
-    putchar('\n');
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
 
-    return 0;
+	return (0);
 }
 
