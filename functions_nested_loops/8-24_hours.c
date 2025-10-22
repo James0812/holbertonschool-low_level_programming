d6dd3457edfa9467eda2f14f16e5e8550d6935f3
James0812 <james.roussel008@gmail.com>
2025-10-22 10:26:13 +0200

    reponse

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -2,22 +2,24 @@
 
 /**
  * jack_bauer - affiche toutes les minutes de la journée
+ *
+ * Return: rien
  */
 void jack_bauer(void)
 {
-    int h, m;
+	int h, m;
 
-    for (h = 0; h < 24; h++)
-    {
-        for (m = 0; m < 60; m++)
-        {
-            _putchar((h / 10) + '0');
-            _putchar((h % 10) + '0');
-            _putchar(':');
-            _putchar((m / 10) + '0');
-            _putchar((m % 10) + '0');
-            _putchar('\n');
-        }
-    }
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
 }
 
