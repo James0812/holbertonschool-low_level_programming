fb331cbde7905323f78d0621cebcf7e1d9cf4ba1
James0812 <james.roussel008@gmail.com>
2025-10-27 12:33:37 +0100

    reponse

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,10 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Declaration of _putchar */
 int _putchar(char c);
-
-/* Declaration of more_numbers */
-void print_square(int size);
+void print_most_numbers(void);
 
 #endif /* MAIN_H */
+
