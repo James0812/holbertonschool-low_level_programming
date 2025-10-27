ae2ce4b6fcf099be8a3ffebb81aee67579af2d5a
James0812 <james.roussel008@gmail.com>
2025-10-27 11:51:13 +0100

    reponse

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -3,24 +3,25 @@
 /**
  * print_square - prints a square using '#' characters
  * @size: size of the square
+ *
+ * Description: prints a square followed by a new line.
+ * If size is 0 or less, prints only a new line.
  */
 void print_square(int size)
 {
-    int i, j;
+	int i, j;
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
 
-    for (i = 0; i < size; i++) /* loop over rows */
-    {
-        for (j = 0; j < size; j++) /* loop over columns */
-        {
-            _putchar('#');
-        }
-        _putchar('\n'); /* new line after each row */
-    }
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+			_putchar('#');
+		_putchar('\n');
+	}
 }
 
