ce1df1a09b4311f92d63d95f8ff24f81cf4da57b
James0812 <james.roussel008@gmail.com>
2025-10-28 13:19:29 +0100

    reponse

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,7 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void _puts(char *str)
+/* Prototypes of common functions */
+int _strlen(char *s);
+void _puts(char *str);
+void _putchar(char c);
+void reset_to_98(int *n);
 
 #endif /* MAIN_H */
 
