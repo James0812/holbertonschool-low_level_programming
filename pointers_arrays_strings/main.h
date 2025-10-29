f20b552b0d9cb98061935b56ca9c96ca8ca15c9d
James0812 <james.roussel008@gmail.com>
2025-10-29 13:27:08 +0100

    reponse

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -4,48 +4,30 @@
 #include <stdio.h>
 
 /* 0x05. Pointers, arrays and strings */
-
-/* Task 0 - reset_to_98 */
 void reset_to_98(int *n);
-
-/* Task 1 - swap_int */
 void swap_int(int *a, int *b);
-
-/* Task 2 - _strlen */
 int _strlen(char *s);
-
-/* Task 3 - _puts */
 void _puts(char *str);
-
-/* Task 4 - print_rev */
 void print_rev(char *s);
-
-/* Task 5 - rev_string */
 void rev_string(char *s);
-
-/* Task 6 - puts2 */
 void puts2(char *str);
-
-/* Task 7 - puts_half */
 void puts_half(char *str);
-
-/* Task 8 - print_array */
 void print_array(int *a, int n);
-
-/* Task 9 - _strcpy */
 char *_strcpy(char *dest, char *src);
-
-/* Task 10 - _atoi */
 int _atoi(char *s);
 
 /* 0x06. More pointers, arrays and strings */
-
-/* Task 0 - _strcat */
 char *_strcat(char *dest, char *src);
-
-/* Task 1 - _strncat */
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *s);
+char *cap_string(char *s);
+char *leet(char *s);
 
-/* Task 2 - _strncpy */
-char *_strncpy(char *dest, char *sr_*
+/* Prototype for _putchar */
+int _putchar(char c);
+
+#endif /* MAIN_H */
 
