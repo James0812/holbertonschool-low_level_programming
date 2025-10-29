c824d07bb91d2cedac13a4d0fdb05fa82bf5fc54
James0812 <james.roussel008@gmail.com>
2025-10-29 10:25:25 +0100

    reponse

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,8 +1,31 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-void reverse_array(int *a, int n);
+#include <stdio.h>
 
-#endif
+/* Prototypes from previous tasks */
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+
+/* Prototypes for string manipulation */
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *s);
+
+/* Provided function */
+int _putchar(char c);
+
+#endif /* MAIN_H */
 
