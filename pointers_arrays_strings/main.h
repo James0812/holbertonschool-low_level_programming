08a501831ebb279f70fc8172a13c0b276902492b
James0812 <james.roussel008@gmail.com>
2025-11-03 09:35:24 +0100

    reponse

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,8 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Function prototype */
-char *_memset(char *s, char b, unsigned int n);
+/**
+ * File: main.h
+ * Auth: Your Name
+ * Desc: Header file containing prototype for function _memcpy.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif /* MAIN_H */
 
