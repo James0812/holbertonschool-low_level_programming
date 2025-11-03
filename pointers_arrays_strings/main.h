768e407e96500cd5e8b0cc061277cf55e0b0ce65
James0812 <james.roussel008@gmail.com>
2025-11-03 13:13:46 +0100

    reponse

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -4,10 +4,10 @@
 /**
  * File: main.h
  * Auth: Your Name
- * Desc: Header file containing prototype for function _strpbrk.
+ * Desc: Header file containing prototype for function _strstr.
  */
 
-char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 
 #endif /* MAIN_H */
 
