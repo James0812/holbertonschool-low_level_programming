96f5cac8941fe69090a203baf6c7ea68d6aa96ff
James0812 <james.roussel008@gmail.com>
2025-11-12 12:23:15 +0100

    reponse

--- structures_typedef/main_0.c
+++ structures_typedef/main_0.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
-#include "main.h"
+#include "main.h"  /* Includes the dog structure and prototype */
 
+/**
+ * main - Tests the init_dog function
+ *
+ * Return: 0
+ */
 int main(void)
 {
-	struct dog my_dog;
+    struct dog my_dog;
 
-	init_dog(&my_dog, "Ghost", 4.75, "Jon Snow");
+    /* Initialize the dog structure */
+    init_dog(&my_dog, "Ghost", 4.75, "Jon Snow");
 
-	printf("Name: %s\n", my_dog.name);
-	printf("Age: %.2f\n", my_dog.age);
-	printf("Owner: %s\n", my_dog.owner);
+    /* Print the values to verify */
+    printf("Name: %s\n", my_dog.name);
+    printf("Age: %.2f\n", my_dog.age);
+    printf("Owner: %s\n", my_dog.owner);
 
-	return (0);
+    return (0);
 }
 
