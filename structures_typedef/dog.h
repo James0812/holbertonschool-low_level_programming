b4412dae1fa43e870605b2825d3a8d0edc0e3461
James0812 <james.roussel008@gmail.com>
2025-11-12 13:55:39 +0100

    reponse

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -9,12 +9,13 @@
  *
  * Description: Structure to store information about a dog.
  */
-struct dog
+typedef struct dog 
 {
 	char *name;
 	float age;
 	char *owner;
 };
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif /* DOG_H */
 
