e570a5430018384ce4f11334ed4b5a100c185a69
James0812 <james.roussel008@gmail.com>
2025-11-12 14:21:53 +0100

    reponse

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,6 +3,33 @@
 #include <stdio.h>
 
 /**
+ * copy_string - allocates memory and copies a string manually
+ * @str: string to copy
+ *
+ * Return: pointer to the new string, or NULL on failure
+ */
+static char *copy_string(char *str)
+{
+	int i, len;
+	char *new_str;
+
+	if (!str)
+		return (NULL);
+
+	for (len = 0; str[len]; len++)
+		;
+
+	new_str = malloc(len + 1);
+	if (!new_str)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+		new_str[i] = str[i];
+
+	return (new_str);
+}
+
+/**
  * new_dog - creates a new dog
  * @name: name of the dog
  * @age: age of the dog
@@ -13,48 +40,22 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
-	char *name_copy = NULL;
-	char *owner_copy = NULL;
-	int i, len;
 
 	d = malloc(sizeof(dog_t));
-	if (d == NULL)
+	if (!d)
 		return (NULL);
 
-	/* copy name manually */
-	if (name)
+	d->name = copy_string(name);
+	d->owner = copy_string(owner);
+	if ((name && !d->name) || (owner && !d->owner))
 	{
-		for (len = 0; name[len]; len++)
-			;
-		name_copy = malloc(len + 1);
-		if (!name_copy)
-		{
-			free(d);
-			return (NULL);
-		}
-		for (i = 0; i <= len; i++)
-			name_copy[i] = name[i];
+		free(d->name);
+		free(d->owner);
+		free(d);
+		return (NULL);
 	}
 
-	/* copy owner manually */
-	if (owner)
-	{
-		for (len = 0; owner[len]; len++)
-			;
-		owner_copy = malloc(len + 1);
-		if (!owner_copy)
-		{
-			free(name_copy);
-			free(d);
-			return (NULL);
-		}
-		for (i = 0; i <= len; i++)
-			owner_copy[i] = owner[i];
-	}
-
-	d->name = name_copy;
 	d->age = age;
-	d->owner = owner_copy;
 
 	return (d);
 }
