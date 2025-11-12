90e51a4ecc799bbf72b9a1972a1aaf26a1fcafe3
James0812 <james.roussel008@gmail.com>
2025-11-12 13:10:40 +0100

    reponse

--- structures_typedef/main_0.c
+++ structures_typedef/main_0.c
@@ -1,3 +1,16 @@
+#include "main.h" 
 #include <stdio.h>
-#include "main.h"
+
+int main(void)
+{
+    struct dog my_dog;
+
+    init_dog(&my_dog, "Ghost", 4.75, "Jon Snow");
+
+    printf("Name: %s\n", my_dog.name);
+    printf("Age: %.2f\n", my_dog.age);
+    printf("Owner: %s\n", my_dog.owner);
+
+    return 0;
+}
 
