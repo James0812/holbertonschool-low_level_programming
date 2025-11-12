07e0920414aadebb866972416a53e82e89277148
James0812 <james.roussel008@gmail.com>
2025-11-12 12:19:21 +0100

    reponse

--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -3,6 +3,12 @@
 
 #include <stddef.h>
 
+/**
+ * struct dog - Structure representing a dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
 struct dog
 {
     char *name;
@@ -10,6 +16,7 @@ struct dog
     char *owner;
 };
 
+/* Prototype for init_dog function */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* MAIN_H */
