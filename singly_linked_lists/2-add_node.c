78a10275cbddbf8c635357b0469566432abe0d0b
James0812 <james.roussel008@gmail.com>
2025-12-01 10:07:30 +0100

    add the new code

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,6 +12,7 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	unsigned int len = 0;
 
 	if (str == NULL)
 		return (NULL);
@@ -27,7 +28,11 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	new_node->len = strlen(str);
+	/* calculate length manually */
+	while (str[len] != '\0')
+		len++;
+
+	new_node->len = len;
 	new_node->next = *head;
 	*head = new_node;
 
