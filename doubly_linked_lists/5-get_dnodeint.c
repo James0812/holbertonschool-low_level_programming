f461d0787defebe6c8a90fbaab35ba6b33957b7e
James0812 <james.roussel008@gmail.com>
2025-12-01 10:54:07 +0100

    add the new code

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -13,12 +13,12 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	while (head != NULL)
 	{
 		if (i == index)
-			return head;
+			return (head);
 
 		head = head->next;
 		i++;
 	}
 
-	return NULL; /* node does not exist */
+	return (NULL);
 }
 
