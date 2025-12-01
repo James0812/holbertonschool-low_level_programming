5fa4df28a116533c027d4552fc67b8d48ce89cfb
James0812 <james.roussel008@gmail.com>
2025-12-01 10:55:44 +0100

    add the new code

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -11,15 +11,16 @@
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 /* Function prototypes */
 void free_dlistint(dlistint_t *head);
 int print_dlistint(const dlistint_t *h);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+int sum_dlistint(dlistint_t *head);
 
 #endif /* LISTS_H */
 
