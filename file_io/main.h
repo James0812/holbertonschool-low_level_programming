b82b3dafc2483e1bb611513f10957640d90bcc99
James0812 <james.roussel008@gmail.com>
2025-12-02 13:52:01 +0100

    add the code at git

--- file_io/main.h
+++ file_io/main.h
@@ -6,7 +6,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif /* MAIN_H */
 
