0c3b144e6b2bd4923fa1708f931977e22ef1c65f
James0812 <james.roussel008@gmail.com>
2025-12-02 09:44:21 +0100

    add the code at git

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,7 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
+#include "main.h"
 
 /**
  * read_textfile - reads a text file and prints it to POSIX standard output
@@ -11,42 +8,42 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t rd, wr;
-    char *buffer;
+	int fd;
+	ssize_t rd, wr;
+	char *buffer;
 
-    if (!filename)
-        return 0;
+	if (!filename)
+		return (0);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return 0;
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-    buffer = malloc(sizeof(char) * letters);
-    if (!buffer)
-    {
-        close(fd);
-        return 0;
-    }
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
+	{
+		close(fd);
+		return (0);
+	}
 
-    rd = read(fd, buffer, letters);
-    if (rd == -1)
-    {
-        free(buffer);
-        close(fd);
-        return 0;
-    }
+	rd = read(fd, buffer, letters);
+	if (rd == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-    wr = write(STDOUT_FILENO, buffer, rd);
-    if (wr != rd)
-    {
-        free(buffer);
-        close(fd);
-        return 0;
-    }
+	wr = write(STDOUT_FILENO, buffer, rd);
+	if (wr != rd)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-    free(buffer);
-    close(fd);
-    return wr;
+	free(buffer);
+	close(fd);
+	return (wr);
 }
 
