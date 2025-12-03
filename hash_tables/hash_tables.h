3f03a79ae66025574a3a38ea826f4731797f49d7
James0812 <james.roussel008@gmail.com>
2025-12-03 09:45:23 +0100

    add the code

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -8,7 +8,7 @@
  * struct hash_node_s - Node of a hash table
  * @key: The key, string
  * @value: The value corresponding to a key
- * @next: A pointer to the next node of the list
+ * @next: A pointer to the next node
  */
 typedef struct hash_node_s
 {
@@ -21,7 +21,6 @@ typedef struct hash_node_s
  * struct hash_table_s - Hash table data structure
  * @size: The size of the array
  * @array: An array of size @size
- * Each cell of this array is a pointer to the first node of a linked list
  */
 typedef struct hash_table_s
 {
@@ -29,7 +28,9 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
+/* Prototypes */
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
 
 #endif /* HASH_TABLES_H */
 
