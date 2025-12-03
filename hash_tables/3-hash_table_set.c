fe807982176885a20be74d6c5c3dda96b13950e4
James0812 <james.roussel008@gmail.com>
2025-12-03 09:49:11 +0100

    add the code

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,6 +1,25 @@
 #include "hash_tables.h"
 
 /**
+ * update_value - updates value if key already exists
+ * @node: current node
+ * @value: new value
+ * Return: 1 on success, 0 on failure
+ */
+int update_value(hash_node_t *node, const char *value)
+{
+	char *value_copy;
+
+	value_copy = strdup(value);
+	if (!value_copy)
+		return (0);
+
+	free(node->value);
+	node->value = value_copy;
+	return (1);
+}
+
+/**
  * hash_table_set - adds an element to the hash table
  * @ht: hash table
  * @key: key
@@ -9,10 +28,8 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node;
-	hash_node_t *current;
+	hash_node_t *new_node, *current;
 	unsigned long int index;
-	char *value_copy;
 
 	if (!ht || !key || !*key)
 		return (0);
@@ -23,15 +40,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	while (current)
 	{
 		if (strcmp(current->key, key) == 0)
-		{
-			value_copy = strdup(value);
-			if (!value_copy)
-				return (0);
-
-			free(current->value);
-			current->value = value_copy;
-			return (1);
-		}
+			return (update_value(current, value));
 		current = current->next;
 	}
 
@@ -40,21 +49,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	new_node->key = strdup(key);
-	if (!new_node->key)
-	{
-		free(new_node);
-		return (0);
-	}
-
-	value_copy = strdup(value);
-	if (!value_copy)
+	new_node->value = strdup(value);
+	if (!new_node->key || !new_node->value)
 	{
 		free(new_node->key);
 		free(new_node);
 		return (0);
 	}
 
-	new_node->value = value_copy;
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
